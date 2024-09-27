a9b1b52fd84f427b19d9d55da1a85e20f184f8f2
JoaoVictor-C <fffjv2006@gmail.com>
2024-09-26 23:23:32 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,24 +1,35 @@
 import axios from 'axios';
 import { isTokenExpired } from '../utils/auth';
-import { useNavigate } from 'react-router-dom';
-
-const navigate = useNavigate();
 
 // Create an axios instance
 const api = axios.create({
   baseURL: 'https://nexuslock-941324057012.southamerica-east1.run.app/api',
 });
 
-// Request interceptor to attach token
+// Request interceptor to attach token and check for token expiration
 api.interceptors.request.use(
   async (config) => {
     const storedAuth = JSON.parse(localStorage.getItem('auth'));
-    if (storedAuth && !await isTokenExpired()) {
+    if (storedAuth) {
       config.headers.Authorization = `Bearer ${storedAuth.token}`;
     }
     return config;
   },
   (error) => Promise.reject(error)
+);
+
+// Response interceptor to handle errors
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const tokenExpired = await isTokenExpired();
+    if (tokenExpired) {
+      localStorage.removeItem('auth');
+      window.location.href = '/login'; 
+      return Promise.reject('Token expired');
+    }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
