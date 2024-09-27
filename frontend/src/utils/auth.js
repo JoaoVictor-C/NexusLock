a1a507d5f61e1d342e6d55dc92f4b02dafab4fa2
João Victor <fffjv2006@gmail.com>
2024-09-26 23:06:14 -0300

    Update auth.js

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const isTokenExpired = async (token) => {
   if (!token) return true;
   try {
-    const response = await axios.post('https://nexuslock-941324057012.southamerica-east1.run.app/api/is-token-valid', { token });
+    const response = await axios.post('https://nexuslock-941324057012.southamerica-east1.run.app/api/Auth/is-token-valid', { token });
     return !response.data.isValid;
   } catch (error) {
     console.error('Error validating token:', error);
